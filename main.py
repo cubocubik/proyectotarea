ce633b0eafc79401e6d1af16d29d13878a21d7b8
cubocubik <a80986360@gmail.com>
2025-07-20 19:32:01 -0500

    Bot que te da memes.py

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
-import discord  # type: ignore
+import discord # type: ignore
 from discord.ext import commands # type: ignore
+import os
 import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True  # Necesario para detectar nuevos miembros
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix ="$", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Conectado como {bot.user}")
 
-# 1. Responder a saludos
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.lower() in ["hola", "holi", "buenas"]:
-        await message.channel.send(f"¡Hola {message.author.name}! 👋")
-    
-    await bot.process_commands(message)  # Importante para que los comandos sigan funcionando
-
-# 2. Comando ping
 @bot.command()
-async def ping(ctx):
-    latency = round(bot.latency * 1000)  # Latencia en ms
-    await ctx.send(f"Pong! 🏓 {latency}ms")
+async def meme(ctx):
+    imagenes = os.listdir("images")
+    imagen_aleatoria = random.choice(imagenes)    
+    with open(f"images/{imagen_aleatoria}", "rb") as f:
+        imagen = discord.File(f)
+    await ctx.send(file = imagen)
 
-# 3. Comando dado
+def obtener_perro():
+    url = "https://dog.ceo/api/breeds/image/random"
+    respuesta = requests.get(url)
+    datos = respuesta.json()
+    return datos["message"]
+
 @bot.command()
-async def dado(ctx):
-    resultado = random.randint(1, 6)
-    await ctx.send(f"🎲 Has sacado un **{resultado}**")
+async def perro(ctx):
+    url_image = obtener_perro()
+    await ctx.send(url_image)
 
-# 4. Mensaje de bienvenida
-@bot.event
-async def on_member_join(member):
-    canal = discord.utils.get(member.guild.text_channels, name="general")  # Cambia si el canal tiene otro nombre
-    if canal:
-        await canal.send(f"👋 ¡Bienvenido/a {member.mention} al servidor!")
-
-bot.run("MTM4OTc2OTgzMTQ0MzU5OTQ5MQ.GkNe7A.E828FLc8U64UTdFeOitw3qtpo9b3Jb6hDbs6H0")
+bot.run("TU_TOKEN_AQUI")
 
